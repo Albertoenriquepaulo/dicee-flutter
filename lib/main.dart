669cdf4ac665ca2ed782dcb0892a45b1d72670b0
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2019-12-23 21:54:17 +0100

    54. Stateful vs. Stateless Widgets - What's the Difference? And setState() Function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,14 +16,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  var random = new Random();
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(child: Image.asset('images/dice1.png')),
-        Expanded(child: Image.asset('images/dice2.png')),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+            onPressed: () {
+              print('Left button got pressed');
+              setState(
+                () {
+                  leftDiceNumber = random.nextInt(5) + 1;
+                },
+              );
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          )),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed');
+              },
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
